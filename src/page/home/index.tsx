141983886be2c72a6d9dbcfe7851f762c49b8d1f
khangnn <khangnn.work@gmail.com>
2026-03-07 00:15:22 +0700

    chore(remove fb cdn)

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,5 +1,4 @@
 import { Flex, Separator } from "@radix-ui/themes";
-import { useEffect } from "react";
 import { AboutUsSection } from "./about-us";
 import { BannerSection } from "./banner";
 import { CallToSupportSection } from "./call-to-support";
@@ -9,20 +8,6 @@ import { TestLocationsSection } from "./test-location";
 import { WhyChooseUsSection } from "./why-choose-us";
 
 export function HomePage() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v25.0";
-    script.async = true;
-    script.defer = true;
-    script.crossOrigin = "anonymous";
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
   return (
     <Flex direction="column" className="bg-[#2B004F]">
       <BannerSection />
